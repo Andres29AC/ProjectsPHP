d6de8e010fa7cceb84b0ac444555bfc816b479c1
Andres Amaro <lionAS_45@hotmail.com>
2024-12-04 04:32:57 -0500

    Update: Add roomDetails part 1

--- sisteHotel/routes/web.php
+++ sisteHotel/routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get("hotels/rooms/{id}",[App\Http\Controllers\Hotels\HotelsController::class,'rooms'])->name('hotels.rooms');
+
+Route::get("hotels/rooms-details/{id}",[App\Http\Controllers\Hotels\HotelsController::class,'roomDetails'])->name('hotel.rooms.details');
