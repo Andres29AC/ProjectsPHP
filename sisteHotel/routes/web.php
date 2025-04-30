1bf4c23d8d22bb30cd6dda629fd15a6f27787ce6
Andres29AC <lionAS_45@hotmail.com>
2025-04-30 15:30:39 -0500

    Add page Admin part 2

--- sisteHotel/routes/web.php
+++ sisteHotel/routes/web.php
@@ -35,3 +35,9 @@ Route::group(['prefix' => 'hotels'], function () {
 
 //NOTE: Users
 Route::get("users/my-bookings",[App\Http\Controllers\Users\UsersController::class,'myBookings'])->name('users.bookings')->middleware('auth:web');
+
+//NOTE: Admin panel
+Route::get('admin/login', [App\Http\Controllers\Admins\AdminsController::class, 'viewLogin'])->name('view.login');
+Route::post('admin/login', [App\Http\Controllers\Admins\AdminsController::class, 'checkLogin'])->name('check.login');
+
+Route::get('admin/dashboard', [App\Http\Controllers\Admins\AdminsController::class, 'index'])->name('admins.dashboard');
